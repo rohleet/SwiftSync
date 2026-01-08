c2fdb31e15546114a30e68f05bf1bb344f9c8b6f
rohleet <rohithakkankol1234@gmail.com>
2026-01-08 18:21:28 +0530

    Added source path validation

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,21 @@ namespace fs = std::filesystem;
 int main(int argc, char* argv[]) {
 
     if (argc != 3) {
-        std::cerr << "Usage: swiftsync <source> <destination>\n";
+        cerr << "Usage: swiftsync <source> <destination>\n";
         return 1;
     }
 
     fs::path source = argv[1];
     fs::path destination = argv[2];
 
-    cout<<argv[1]<<endl;
-    cout<<argv[2]<<endl;
+    if (!fs::exists(source) || !fs::is_directory(source)) {
+        if(!fs::exists(source)){
+            cerr<<"Source directory does not exist";
+        } else {
+            cerr<<"The given path is not a directory";
+        }
+        return 1;
+    }
     
     return 0;
 }
